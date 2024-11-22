59b9bd9bdfdeadf0f2eb7187c24b66dda6e4d1a8
Danishwara Pracheta <danishwara81@gmail.com>
2024-11-22 16:51:51 +0800

    visual changes

--- tbo/uts/main.py
+++ tbo/uts/main.py
@@ -1,4 +1,4 @@
-from nfa import nfa
+from nfa import nfa, delta_nfa
 
 
 def test_nfa():
@@ -13,10 +13,20 @@ def test_nfa():
     ]
 
     for i, (languages, expected) in enumerate(test_cases, 1):
+        states = {'q0'}
         result = nfa(languages)
-        print(f"Test Case {i}: {'Passed' if result == expected else 'Failed'}")
+        print(f"Test Case {i}: {'Passed' if result == expected else 'Failed'}", end="")
         if result != expected:
             print(f"    Input: {languages}")
             print(f"    Expected: {expected}, Got: {result}")
+        print(f" | States Transition: {states}", end="")
+        for language in languages:
+            curr = set()
+            for state in states:
+                curr = curr.union(delta_nfa(state, language))
+            print(f" --> {curr}", end="")
+            states = curr
+        print()
+
 
 test_nfa()
