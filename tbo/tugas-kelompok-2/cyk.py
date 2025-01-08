d3e58524b77c231c32c4520f7e36581fbfae2af2
Danishwara Pracheta <danishwara81@gmail.com>
2025-01-08 22:10:06 +0800

    updated

--- tbo/tugas-kelompok-2/cyk.py
+++ tbo/tugas-kelompok-2/cyk.py
@@ -42,18 +42,18 @@ R = parse_grammar(grammar)
 # Function to perform the CYK Algorithm
 def cykParse(w):
     n = len(w)
-# Initialize the table
+    # Initialize the table
     T = [[set([]) for j in range(n)] for i in range(n)]
-# Filling in the table
+    # Filling in the table
     for j in range(0, n):
-# Iterate over the rules
+        # Iterate over the rules
         for lhs, rule in R.items():
             for rhs in rule:
-# If a terminal is found
+                # If a terminal is found
                 if len(rhs) == 1 and rhs[0] == w[j]:
                     T[j][j].add(lhs)
         for i in range(j, -1, -1):
-    # Iterate over the range i to j-1 (inclusive)
+            # Iterate over the range i to j-1 (inclusive)
             for k in range(i, j):
                 # Iterate over the rules
                 for lhs, rule in R.items():
@@ -61,6 +61,6 @@ def cykParse(w):
                         if len(rhs) == 2 and rhs[0] in T[i][k] and rhs[1] in T[k+1][j]:
                             T[i][j].add(lhs)
 
-# If word can be formed by rules
-# of given grammar
-    return len(T[0][n-1]) != 0, T
+    # If word can be formed by rules
+    # of given grammar
+    return 'K' in T[0][n-1], T
