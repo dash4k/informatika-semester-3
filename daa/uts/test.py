77c4bb29cf58c34376c3caa467415a56b4d83e18
Danishwara Pracheta <danishwara81@gmail.com>
2025-09-07 20:32:46 +0800

    added README.md

--- daa/uts/test.py
+++
@@ -1,5 +0,0 @@
-def x(n):
-    # return ((((n-1) * n) // 2) + 3 * (n-1))
-    return ((n - 1) * n) // 2
-
-print(x(1000))
--- pbo/quiz-1/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Chef budi = new Chef("Budi", "Machete");
-        budi.duty();
-        budi.cook();
-        budi.rest();   
-    }
-}
